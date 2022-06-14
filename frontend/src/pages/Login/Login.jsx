a0a1d4b5d1cdb126d3a4ea1ba803bb1ad6052832
muhreynaldii <reynaldi180101@gmail.com>
2022-06-14 15:16:14 +0700

    add login

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import bg from "../../assets/images/bg_login.png";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+
+  const Auth = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/login", {
+        username: username,
+        password: password,
+      });
+      navigate("/admin");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
   return (
     <div
       className="h-screen w-full overflow-hidden bg-cover bg-center bg-no-repeat bg-origin-border"
@@ -9,7 +30,11 @@ function Login() {
     >
       <div className="mx-auto my-12 flex h-[546px] w-[451px] flex-col self-center rounded-md bg-[#FFFDFDCF] p-10 shadow-lg">
         <h1 className="text-center text-2xl text-black">Login</h1>
-        <div className="flex flex-col justify-center space-y-2 pt-14">
+        <form
+          onSubmit={Auth}
+          className="flex flex-col justify-center space-y-2 pt-14"
+        >
+          <p className="my-2 text-center">{msg}</p>
           <label htmlFor="username" className="py-1">
             Username
           </label>
@@ -17,7 +42,9 @@ function Login() {
             type="text"
             name="username"
             id="username"
-            className="h-[53px] rounded-sm bg-grey-custom"
+            className="bg-grey-custom h-[53px] rounded-sm"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <label htmlFor="password" className="py-1">
             Password
@@ -26,17 +53,17 @@ function Login() {
             type="password"
             name="password"
             id="password"
-            className="h-[53px] rounded-sm bg-grey-custom"
+            className="bg-grey-custom h-[53px] rounded-sm"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-        </div>
-        <div className="mt-14 flex justify-between">
-          <button className="w-[120px] bg-orange-custom px-3 py-2 text-base font-normal text-white">
-            Cancel
-          </button>
-          <button className="w-[120px] bg-green-custom px-3 py-2 text-base font-normal text-white">
-            Login
-          </button>
-        </div>
+
+          <div className="mt-14 flex justify-between">
+            <button className="w-[120px] bg-green-custom px-3 py-2 text-base font-normal text-white">
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
